652119791a0eed4bc5575229e3a9a1f8706fe5fa
Hong Doan <dtthong@tma.com.vn>
2023-11-08 18:38:06 +0700

    UI Customer

---
+++ FinalWeb/index.js
@@ -0,0 +1,8 @@
+$(function () {
+  $(".navbar-container").load("./Component/header.html");
+  $("footer").load("./Component/footer.html");
+  $(".about-me").load("./Component/about.html");
+  $(".slider").load("./Component/slideshow.html");
+  $(".other-service").load("./Component/otherService.html");
+  $(".customer").load("./Component/customer.html");
+});
