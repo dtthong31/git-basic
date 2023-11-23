e328f81aa309b84b160cf0eb824238e64c5b2b47
dtthong31 <hongblack2000@gmail.com>
2023-11-23 23:43:17 +0700

    Final website

--- FinalWeb/index.js
+++ FinalWeb/index.js
@@ -5,5 +5,30 @@ $(function () {
   $(".slider").load("./Component/Slideshow.html");
   $(".other-service").load("./Component/OtherService.html");
   $(".customer").load("./Component/Customer.html");
-  $(".info").load("./Component/NumberCounter.html");
+  $(".info").load("./Component/Info.html");
 });
+
+// const navbarContainer = document.querySelector('.navbar-container');
+// const footer = document.querySelector('footer');
+// const aboutMe = document.querySelector('.about-me');
+// const slider = document.querySelector('.slider');
+// const otherService = document.querySelector('.other-service');
+// const customer = document.querySelector('.customer');
+// const info = document.querySelector('.info');
+
+// const observer = new IntersectionObserver(entries => {
+//   entries.forEach(entry => {
+//     if (entry.isIntersecting) {
+//       entry.target.classList.add('animate');
+//       observer.unobserve(entry.target);
+//     }
+//   });
+// });
+
+// observer.observe(navbarContainer);
+// observer.observe(footer);
+// observer.observe(aboutMe);
+// observer.observe(slider);
+// observer.observe(otherService);
+// observer.observe(customer);
+// observer.observe(info);
